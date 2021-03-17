10fd3ce1040994ba5ae9e49d5705b3541c4fb440
Ethan Feldman <ejfeldman7@gmail.com>
2021-03-17 11:26:05 -0500

    Update pizza_vision.py

--- web_app/pizza_vision.py
+++ web_app/pizza_vision.py
@@ -175,7 +175,7 @@ st.title("Upload + Classification Example")
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-if (uploaded_file is not None & (user_text != '')):
+if ((uploaded_file is not None) & (user_text != '')):
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
